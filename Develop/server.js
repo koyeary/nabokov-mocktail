243e43ccc2348425750535d629b5f8e58bf9038a
Kat Yeary <katyeary@gmail.com>
2020-06-17 16:05:59 -0700

    stringify updated db variable

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,11 @@
 // Dependencies
 const express = require('express');
-const path = require("path");
-const db = require("../Develop/db/db.json");
+const path = require('path');
+const db = require('../Develop/db/db.json');
+const fs = require('fs');
+
 const port = process.env.PORT || 8080; 
 const app = express(); 
-
-/* module.exports = function (app) { */
 
 // Set us up the Express App
 app.use(express.urlencoded({ extended: true })); // Sets up the Express app to handle data parsing
@@ -13,13 +13,13 @@ app.use(express.json());
 
 //Route that sends the user to the homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"))
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 });
 
 
 //Route that sends user to the notes page
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/notes.html"))
+  res.sendFile(path.join(__dirname + '/public/notes.html'))
 });
 
 
@@ -28,10 +28,10 @@ app.get('/api/notes', (req, res) => {
   return res.json(db);
 });
 
-app.get("api/notes/:note", (req, res) => {
+app.get('api/notes/:note', (req, res) => {
   const noteReq = req.params.note;
 
-  console.log(noteReq);
+  //console.log(noteReq);
 
   for (let i = 0; i < notes.length; i++) {
     if (noteReq === db[i].routeName) {
@@ -43,21 +43,23 @@ app.get("api/notes/:note", (req, res) => {
 
 //Creates new notes, takes in JSON input
 app.post('/api/notes', (req, res) => {
-  var newNote = req.body;
+  let newNote = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newNote
-   newNote.title = newNote.name.replace(/\s+/g, "").toLowerCase();
+/*   // Using a RegEx Pattern to remove spaces from newNote
+   newNote.title = newNote.title.replace(/\s+/g, '').toLowerCase(); */
  
-  console.log(newNote);
-
-  db.push(newNote);
-
   res.json(newNote);
-
+  db.push(newNote);
+  let updateDb = JSON.stringify(db);
+  
+   fs.writeFile('../Develop/db/db.json', updateDb, function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  })  
 });
  
 //Starts the server
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
-/* } */
+
