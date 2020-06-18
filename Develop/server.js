aef45004d4f757d50d94ec9b54616efa43bf1244
Kat Yeary <katyeary@gmail.com>
2020-06-18 13:14:36 -0700

    Server hears delete requests

--- Develop/server.js
+++ Develop/server.js
@@ -7,8 +7,8 @@ const fs = require('fs');
 const port = process.env.PORT || 8080; 
 const app = express(); 
 
-// Set us up the Express App
-app.use(express.urlencoded({ extended: true })); // Sets up the Express app to handle data parsing
+// Sets up the Express App
+app.use(express.urlencoded({ extended: true })); //handles data parsing
 app.use(express.json()); 
 
 //Route that sends the user to the homepage
@@ -28,26 +28,11 @@ app.get('/api/notes', (req, res) => {
   return res.json(db);
 });
 
-/* app.get('api/notes/:note', (req, res) => {
-  const noteReq = req.params.note;
 
-  //console.log(noteReq);
-
-  for (let i = 0; i < notes.length; i++) {
-    if (noteReq === db[i].routeName) {
-      return res.json(db[i]);
-    }
-    return res.json(false);
-  } 
-})
- */
 //Creates new notes, takes in JSON input
 app.post('/api/notes', (req, res) => {
   let newNote = req.body;
 
-/*   // Using a RegEx Pattern to remove spaces from newNote
-   newNote.title = newNote.title.replace(/\s+/g, '').toLowerCase(); */
- 
   res.json(newNote);
   db.push(newNote);
   let updateDb = JSON.stringify(db);
@@ -68,20 +53,18 @@ app.get('/api/notes/:id', (req, res) => {
   }
   res.status(404).send(`Sorry, we can't find a note matching ID #${noteReq}`);
 });
-  //res.end();
-/*   for (var i = 0; i < db.length; i++) {
-     if (noteReq === db[i].id) {
-       return console.log(db[i]);
-        res.send(db[i]);
-      return res.json(db[i]); 
-      
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteReq = parseInt(req.params.id);
+  res.send(`Got a DELETE request at /${noteReq}`)
+  /* console.log(noteReq);
+  for (var note of db) {
+    if (note.id === noteReq) {
+     return res.send(note);
     }
-    res.send(db[i]); 
-  } */
- // res.status(404).send(`Sorry, we can't find that!`);  
-
-
-
+  }
+  res.status(404).send(`Sorry, we can't find a note matching ID #${noteReq}`); */
+});
 
 //Starts the server
 app.listen(port, () => {
